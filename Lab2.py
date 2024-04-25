b10dcbdaeebe14921c3bb3fa73c9d30d0a5ff88a
bry <bryanong.23@ichat.sp.edu.sg>
2024-04-25 16:59:40 +0800

    bug patch

--- Lab2.py
+++ Lab2.py
@@ -1,14 +1,14 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
 def display_main_menu():    
-    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+    print("Enter some numbers separated by commas (e.g. 4, 20, 69)")
 
 def get_user_input():    
     numberInput = input()
-    numberList = numberInput.split()
-    numberList = [float(num) for num in numberList]
+    inputList = numberInput.split(",")
+    inputList = [float(num) for num in inputList]
 
-    return numberList
+    return inputList
 
 def calc_average(inputList):
     inputSum = 0
@@ -30,7 +30,7 @@ def find_min_max(inputList):
 
     return inputMin, inputMax
 
-def sortList(inputList):    
+def sort_List(inputList):    
     return inputList.sort()
 
 def calc_median(inputList):
@@ -40,3 +40,16 @@ def calc_median(inputList):
         inputMedian = (inputList[len(inputList)/2] + inputList[len(inputList)/2 + 1]) / 2
     
     return inputMedian
+
+display_main_menu()
+numberList = get_user_input()
+
+numberAverage = calc_average(numberList)
+print("average is " + str(numberAverage))
+numberMin = find_min_max(numberList)[0]
+print("min value is " + str(numberMin))
+numberMax = find_min_max(numberList)[1]
+print("max value is " + str(numberMax))
+ascendingNumberList = sort_List(numberList)
+numberMedian = calc_median(ascendingNumberList)
+print("median value is " + str(numberMedian))
