ef6c18dbadc28a5c9a92a696099a2d3e47122449
bry <bryanong.23@ichat.sp.edu.sg>
2024-04-25 16:15:38 +0800

    added code

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,37 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-def funcName(parameter1, parameter2):     
-    print("this is a dummy function")     
-    return 10 
+
+def display_main_menu():    
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():    
+    numberInput = input()
+    numberList = numberInput.split()
+    numberList = [float(num) for num in numberList]
+
+    return numberList
+
+def calc_average(inputList):
+    inputSum = 0
+    for num in inputList:
+        inputSum += num
+    
+    return inputSum
+
+def find_min_max(inputList):
+    inputMin = float('inf')
+    for num in inputList:
+        if (num < inputMin):
+            inputMin = int(num)
+
+    inputMax = float('-inf')
+    for num in inputList:
+        if (num > inputMax):
+            inputMax = int(num)
+
+    return inputMin, inputMax
+
+def sort():    
+    print("sort_temperature")
+
+def calc():    
+    print("calc_median_temperature")
