3bd8fc19d7c3e67c88cf5ec0c342e665ea7b6a80
bry <bryanong.23@ichat.sp.edu.sg>
2024-05-02 14:51:12 +0800

    added the return values

--- bmi.py
+++ bmi.py
@@ -2,16 +2,19 @@ def calculate_bmi(height, weight):
     bmi = weight / height / height
     if(bmi > 25):   
         weight_class = "overweight! (Eat more veggies)"
+        return_value = 1
     elif(bmi < 18.5):   
         weight_class = "underweight! (You are a toothpick)"
+        return_value = 0
     else:   
-        eight_class = "normal weight. (You are great)"
+        weight_class = "normal weight. (You are great)"
+        return_value = -1
 
     print("Your weight is " + str(weight) + "kg")
     print("Your height is " + str(height) + "m")
     print("Your BMI is " + str(bmi))
     print("Therefore, you are " + weight_class)
-    return
+    return return_value
 
 calculate_bmi(weight=57,height=1.73)
 
